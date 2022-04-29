211804560ee6a0541e9f88b8b6a22cac7f153b27
TyzuaL <tyzual@outlook.com>
2022-04-30 00:37:31 +0800

    增加错误码单测

---
+++ test/error_code_test.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+#include <type_traits>
+
+#include "salt/error.h"
+
+TEST(salt_error_test, test_error_catetory) {
+  auto error_code = salt::make_error_code(salt::error_code::success);
+  ASSERT_EQ(error_code.category(), salt::error_category());
+  ASSERT_EQ(error_code.value(),
+            static_cast<std::underlying_type_t<salt::error_code>>(
+                salt::error_code::success));
+}
