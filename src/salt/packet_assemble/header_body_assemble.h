3123ee34f0cb2773ce6e01f0bf21b41e7a482b57
tyzual <tyzual@outlook.com>
2022-05-13 23:11:12 +0800

    调整写法

--- src/salt/packet_assemble/header_body_assemble.h
+++ src/salt/packet_assemble/header_body_assemble.h
@@ -20,78 +20,7 @@ public:
                 "Header has padding");
 
   data_read_result data_received(std::shared_ptr<connection_handle> connection,
-                                 std::string s) override {
-
-    uint32_t offset{0};
-    auto rest_data_length = s.size();
-    while (offset < s.size()) {
-      switch (current_stat_) {
-      case ParseStat::kHeader: {
-        if (rest_length_ > rest_data_length) {
-          rest_length_ -= rest_data_length;
-          header_ += s.substr(offset);
-          return data_read_result::disconnect;
-        } else if (rest_length_ == rest_data_length) {
-          header_ += std::move(s);
-          body_size_ =
-              reinterpret_cast<HeaderType *>(header_.data())->*length_property;
-          rest_length_ = body_size_;
-          current_stat_ = ParseStat::kBody;
-          return data_read_result::disconnect;
-        } else /* if (rest_length_ < rest_data_length) */ {
-          rest_data_length -= rest_length_;
-          header_ += s.substr(offset, rest_length_);
-          offset += rest_length_;
-          body_size_ =
-              reinterpret_cast<HeaderType *>(header_.data())->*length_property;
-          body_.clear();
-          body_.reserve(body_size_);
-          rest_length_ = body_size_;
-          current_stat_ = ParseStat::kBody;
-        }
-      }
-        [[fallthrough]];
-      case ParseStat::kBody: {
-        if (rest_data_length < rest_length_) {
-          body_ += s.substr(offset);
-          rest_length_ = body_size_ - rest_data_length;
-          current_stat_ = ParseStat::kBody;
-          return data_read_result::disconnect;
-        } else if (rest_data_length == rest_length_) {
-          body_ += s.substr(offset);
-          // TODO 通知上游，收到新包
-          log_debug("get message body size:%llu, content:%s", body_size_,
-                    body_.c_str());
-          current_stat_ = ParseStat::kHeader;
-          rest_length_ = header_size_;
-          header_.clear();
-          body_.clear();
-          body_size_ = 0;
-          return data_read_result::disconnect;
-        } else /* if (rest_data_length > rest_length_) */ {
-          rest_data_length -= rest_length_;
-          body_ += s.substr(offset, rest_length_);
-          offset += rest_length_;
-          // TODO 通知上游，收到新包
-          log_debug("get message body size:%llu, content:%s", body_size_,
-                    body_.c_str());
-          current_stat_ = ParseStat::kHeader;
-          rest_length_ = header_size_;
-          header_.clear();
-          body_.clear();
-          body_size_ = 0;
-        }
-      } break;
-      }
-    }
-
-    return data_read_result::disconnect;
-  }
-
-  void _test(void *buff) {
-    std::cout << reinterpret_cast<HeaderType *>(buff)->*length_property
-              << std::endl;
-  }
+                                 std::string s) override;
 
   ~header_body_assemble() = default;
 
@@ -109,5 +38,81 @@ private:
   ParseStat current_stat_{ParseStat::kHeader};
   uint32_t rest_length_{header_size_};
 };
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// 模板函数实现
+///////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename HeaderType, typename SizeType,
+          SizeType HeaderType::*length_property>
+data_read_result
+header_body_assemble<HeaderType, SizeType, length_property>::data_received(
+    std::shared_ptr<connection_handle> connection, std::string s) {
+  uint32_t offset{0};
+  auto rest_data_length = s.size();
+  while (offset < s.size()) {
+    switch (current_stat_) {
+    case ParseStat::kHeader: {
+      if (rest_length_ > rest_data_length) {
+        rest_length_ -= rest_data_length;
+        header_ += s.substr(offset);
+        return data_read_result::disconnect;
+      } else if (rest_length_ == rest_data_length) {
+        header_ += std::move(s);
+        body_size_ =
+            reinterpret_cast<HeaderType *>(header_.data())->*length_property;
+        rest_length_ = body_size_;
+        current_stat_ = ParseStat::kBody;
+        return data_read_result::disconnect;
+      } else /* if (rest_length_ < rest_data_length) */ {
+        rest_data_length -= rest_length_;
+        header_ += s.substr(offset, rest_length_);
+        offset += rest_length_;
+        body_size_ =
+            reinterpret_cast<HeaderType *>(header_.data())->*length_property;
+        body_.clear();
+        body_.reserve(body_size_);
+        rest_length_ = body_size_;
+        current_stat_ = ParseStat::kBody;
+      }
+    }
+      [[fallthrough]];
+    case ParseStat::kBody: {
+      if (rest_data_length < rest_length_) {
+        body_ += s.substr(offset);
+        rest_length_ = body_size_ - rest_data_length;
+        current_stat_ = ParseStat::kBody;
+        return data_read_result::disconnect;
+      } else if (rest_data_length == rest_length_) {
+        body_ += s.substr(offset);
+        // TODO 通知上游，收到新包
+        log_debug("get message body size:%llu, content:%s", body_size_,
+                  body_.c_str());
+        current_stat_ = ParseStat::kHeader;
+        rest_length_ = header_size_;
+        header_.clear();
+        body_.clear();
+        body_size_ = 0;
+        return data_read_result::disconnect;
+      } else /* if (rest_data_length > rest_length_) */ {
+        rest_data_length -= rest_length_;
+        body_ += s.substr(offset, rest_length_);
+        offset += rest_length_;
+        // TODO 通知上游，收到新包
+        log_debug("get message body size:%llu, content:%s", body_size_,
+                  body_.c_str());
+        current_stat_ = ParseStat::kHeader;
+        rest_length_ = header_size_;
+        header_.clear();
+        body_.clear();
+        body_size_ = 0;
+      }
+    } break;
+    }
+  }
+
+  return data_read_result::disconnect;
+}
 
 } // namespace salt
